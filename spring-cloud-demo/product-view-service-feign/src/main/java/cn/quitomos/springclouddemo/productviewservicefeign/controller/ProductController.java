eb303dd04175ab798993d9f6b97280dfd0dc585f
Vane <1658073818@qq.com>
2021-06-10 11:02:05 +0800

    修改了 spring cloud demo 的一些配置

--- spring-cloud-demo/product-view-service-feign/src/main/java/cn/quitomos/springclouddemo/productviewservicefeign/controller/ProductController.java
+++ spring-cloud-demo/product-view-service-feign/src/main/java/cn/quitomos/springclouddemo/productviewservicefeign/controller/ProductController.java
@@ -3,19 +3,28 @@ package cn.quitomos.springclouddemo.productviewservicefeign.controller;
 import cn.quitomos.springclouddemo.productviewservicefeign.entity.Product;
 import cn.quitomos.springclouddemo.productviewservicefeign.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class ProductController {
 
     private ProductService productService;
 
+    @Value("${version}")
+    private String version;
+
     @GetMapping("/product")
-    public List<Product> listProducts() {
-        return productService.listProducts();
+    public Map<String, Object> listProducts() {
+        Map<String, Object> ret = new HashMap<>();
+        ret.put("version", version);
+        ret.put("products", productService.listProducts());
+        return ret;
     }
 
     @Autowired
